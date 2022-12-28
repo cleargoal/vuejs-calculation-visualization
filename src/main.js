848964a15a6f2d24630b8c194f9c601ef5ddaf11
best <cleargoal01@gmail.com>
2022-12-28 21:12:08 +0100

    add pinia to main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
+const app = createApp(App)
+const pinia = createPinia()
 import './assets/main.css'
 
-createApp(App).mount('#app')
+app.use(pinia)
+app.mount('#app')
